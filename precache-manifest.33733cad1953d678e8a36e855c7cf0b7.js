734e2ca214af9511198770b8ad8fe993451f1829
Sergei Kurochkin <se.kurochkin@x5.ru>
2020-06-25 20:24:00 +0300

    Updates

---
+++ precache-manifest.33733cad1953d678e8a36e855c7cf0b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31e26d5072f1f87b01da621e09c243f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9f91edc5ba0bee6b910",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a669ef451d70757e657f",
+    "url": "/static/js/2.509106d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.509106d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f91edc5ba0bee6b910",
+    "url": "/static/js/main.ff08b44a.chunk.js"
+  },
+  {
+    "revision": "dbf65702745a6d347b70",
+    "url": "/static/js/runtime-main.9263c675.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
