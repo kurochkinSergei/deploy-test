47ec0af364ec327d9dd6ad577b3fdadac2e60ee5
Sergei Kurochkin <se.kurochkin@x5.ru>
2020-06-25 20:28:18 +0300

    Updates

---
+++ precache-manifest.b5520ba0869a8b2f1b43919b6e9887ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27357aafd68efbf8ccb91d7b001e642",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "f2ba3dfb2328dabb2243",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a669ef451d70757e657f",
+    "url": "/deploy-test/static/js/2.509106d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.509106d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ba3dfb2328dabb2243",
+    "url": "/deploy-test/static/js/main.3f43bb2f.chunk.js"
+  },
+  {
+    "revision": "2a8aa1ed27513d4f5287",
+    "url": "/deploy-test/static/js/runtime-main.2c42775a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33733cad1953d678e8a36e855c7cf0b7.js"
+  "/deploy-test/precache-manifest.b5520ba0869a8b2f1b43919b6e9887ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
